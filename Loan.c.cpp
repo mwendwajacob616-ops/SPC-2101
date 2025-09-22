ede39faf078f56557a98817cf4f77acab461ed4c
mwendwajacob616-ops <mwendwajacob616@gmail.com>
2025-09-23 00:30:28 +0300

    Update Loan.c.cpp

--- Loan.c.cpp
+++ Loan.c.cpp
@@ -7,11 +7,8 @@ scanf("%d", &age);
 printf("Enter your yearly income(in shillings): ");
 scanf("%f", &income);
 if(age>=21 && income>=21000)
-{
 printf("Congratulations,you qualify for a loan");
-}
-else {
+else 
 printf("Unfortunately,we are unable to offer you a loan at this time");
-}
 return 0;
 }
