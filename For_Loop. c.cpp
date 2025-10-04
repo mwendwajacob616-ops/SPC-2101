098102d73979f620e258288c15126d5b1219595e
mwendwajacob616-ops <mwendwajacob616@gmail.com>
2025-10-04 06:46:53 +0300

    Add files via upload

---
+++ For_Loop. c.cpp
@@ -0,0 +1,14 @@
+/*
+Name:Jacob Mwendwa Nzau
+ADM no.:CT101/G/26517/25
+Description:Using For loop to print all numbers between 100 and 50 in descending order.
+*/
+#include <stdio.h>
+int main(){
+int i;
+for(i=100; i>=50; i--)
+{
+printf("%d\n", i);
+}
+return 0;
+}
