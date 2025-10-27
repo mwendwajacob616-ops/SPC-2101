9597cb94fadcdccae62077a6eabb9e2987d42116
Jacob_Mwendwa <mwendwajacob616@gmail.com>
2025-10-27 20:10:27 +0300

    Add files via upload

---
+++ convertToCelcius().c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+float convertToCelsius(float fahrenheit) {
+    float celsius;
+    celsius = (fahrenheit - 32) * 5.0 / 9.0;
+    return celsius;
+}
+int main() {
+    float F, C;
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &F);
+    C = convertToCelsius(F);
+    printf("Temperature in Celsius: %.2f\n", C);
+    return 0;
+}
